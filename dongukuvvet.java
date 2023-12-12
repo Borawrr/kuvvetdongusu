5892f1c9f716915f037851a91308586ad443bc87
Bora Tekin <pikacu1903@gmail.com>
2023-12-12 17:38:26 +0300

    Create dongukuvvet.java

---
+++ dongukuvvet.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class dongukuvvet {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+        
+        System.out.println("4'ün ve 5'in kuvvetleri:");
+
+        for (int i = 0; i <= sayi; i++) {
+            
+            System.out.println("4^" + i + " = " + (int) Math.pow(4, i));
+            System.out.println("5^" + i + " = " + (int) Math.pow(5, i));
+        
+
+
+
+
+
+
+
+        
+    }
+}
+}
